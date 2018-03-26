adef7010c439e82c000e10e86b938be51234c8b5
amaciukaite <agne.strategeens@gmail.com>
2018-03-26 23:17:55 +0300

    remove unnecessary folders

--- crawler-etherscan/index.js
+++
@@ -1,6 +0,0 @@
-const CrawlerService = require ('./service/crawler-service');
-const AddressesToCodeService = require ('./service/addresses-to-code-service');
-
-// module.exports = AddressesToCodeService.convert('./addresses.txt');
-
-module.exports = CrawlerService.crawEtherscan();
